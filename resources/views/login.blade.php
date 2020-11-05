ab538b00b473e8b93ac961145df2bfb65ba892f3
ismdeep <ismdeep@protonmail.com>
2020-11-05 22:23:07 +0800

    refactor: login.blade.php, register.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,34 +1,30 @@
 @extends('_common_')
 
 @section('body')
-    <div class="hero is-primary is-fullheight">
-        <div class="hero-body">
-            <div class="container">
-                <div class="columns is-centered">
-                    <div class="column is-5-tablet is-4-desktop is-3-widescreen">
-                        <form class="box" action="{{ url('/login') }}" method="post">
-                            @csrf
-                            <div class="field">
-                                <label class="label" for="username">Username</label> <input class="input" type="text"
-                                                                                            id="username"
-                                                                                            name="username"
-                                                                                            placeholder="Username">
-                            </div>
-                            <div class="field">
-                                <label class="label" for="password">Password</label> <input class="input"
-                                                                                            type="password"
-                                                                                            id="password"
-                                                                                            name="password"
-                                                                                            placeholder="Password">
-                            </div>
-                            <div class="field">
-                                <div class="control">
-                                    <button class="button is-link">Login</button>
-                                </div>
-                            </div>
-                        </form>
+    <div class="container">
+        <div class="columns is-centered">
+            <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+                <form class="box" action="{{ url('/login') }}" method="post">
+                    @csrf
+                    <div class="field">
+                        <label class="label" for="username">Username</label> <input class="input" type="text"
+                                                                                    id="username"
+                                                                                    name="username"
+                                                                                    placeholder="Username">
                     </div>
-                </div>
+                    <div class="field">
+                        <label class="label" for="password">Password</label> <input class="input"
+                                                                                    type="password"
+                                                                                    id="password"
+                                                                                    name="password"
+                                                                                    placeholder="Password">
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-link">Login</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,55 +1,48 @@
 @extends('_common_')
 
 @section('body')
-
-    <div class="hero is-primary is-fullheight">
-        <div class="hero-body">
-            <div class="container">
-                <div class="columns is-centered">
-                    <div class="column is-5-tablet is-4-desktop is-3-widescreen">
-                        <form class="box" action="{{ url('/register') }}" method="post">
-                            @csrf
-                            <div class="field">
-                                <label class="label" for="username">Username</label>
-                                <input class="input" type="text"
-                                       id="username"
-                                       name="username"
-                                       placeholder="Username">
-                            </div>
-                            <div class="field">
-                                <label class="label" for="email">Email</label>
-                                <input class="input" type="text"
-                                       id="email" name="email"
-                                       placeholder="Email">
-                            </div>
-                            <div class="field">
-                                <label class="label" for="password">Password</label>
-                                <input class="input"
-                                       type="password"
-                                       id="password"
-                                       name="password"
-                                       placeholder="Password">
-                            </div>
-                            <div class="field">
-                                <label class="label" for="password-confirm">Password Confirm</label>
-                                <input
-                                    class="input"
-                                    type="password"
-                                    id="password-confirm"
-                                    name="password-confirm"
-                                    placeholder="Password Confirm">
-                            </div>
-                            <div class="field">
-                                <div class="control">
-                                    <button class="button is-link">Register</button>
-                                </div>
-                            </div>
-                        </form>
+    <div class="container">
+        <div class="columns is-centered">
+            <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+                <form class="box" action="{{ url('/register') }}" method="post">
+                    @csrf
+                    <div class="field">
+                        <label class="label" for="username">Username</label>
+                        <input class="input" type="text"
+                               id="username"
+                               name="username"
+                               placeholder="Username">
                     </div>
-                </div>
+                    <div class="field">
+                        <label class="label" for="email">Email</label>
+                        <input class="input" type="text"
+                               id="email" name="email"
+                               placeholder="Email">
+                    </div>
+                    <div class="field">
+                        <label class="label" for="password">Password</label>
+                        <input class="input"
+                               type="password"
+                               id="password"
+                               name="password"
+                               placeholder="Password">
+                    </div>
+                    <div class="field">
+                        <label class="label" for="password-confirm">Password Confirm</label>
+                        <input
+                            class="input"
+                            type="password"
+                            id="password-confirm"
+                            name="password-confirm"
+                            placeholder="Password Confirm">
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-link">Register</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-
-
 @endsection
