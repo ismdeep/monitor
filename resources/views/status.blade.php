779445b022fb7a67d4f28bea20a69fd7d16c68c5
ismdeep <l.jiang.1024@gmail.com>
2020-11-18 19:32:08 +0800

    fix a problem in status.blade.php

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -16,7 +16,7 @@
                 let html = '';
                 for (let i = 0; i < d.data.length; i++) {
                     let tag_class = d.data[i].is_alive ? 'mdui-color-green' : 'mdui-color-red';
-                    html += '<div class="mdui-chip mdui-p-x-1">' +
+                    html += '<div class="mdui-chip mdui-m-x-1">' +
                         '<a class="mdui-text-color-grey" href="/status/' + d.data[i].id + '">' +
                         '<span class="mdui-chip-title">' + d.data[i].key + '</span>' +
                         '<span class="mdui-chip-icon ' + tag_class + '">' +
