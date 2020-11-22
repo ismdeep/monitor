858e2a25c2fa9a9e9777f6176c509b1ae1188e39
ismdeep <l.jiang.1024@gmail.com>
2020-11-22 11:01:36 +0800

    refactor: status.blade.php

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -16,7 +16,9 @@
                 let html = '';
                 for (let i = 0; i < d.data.length; i++) {
                     let tag_class = d.data[i].is_alive ? 'mdui-text-color-green' : 'mdui-text-color-red';
-                    html += '<a style="padding: 10px;text-decoration: none" class="' + tag_class + '" href="/status/' + d.data[i].id + '">' + d.data[i].key + '[' + d.data[i].ago_text + ']' + '</a>';
+                    html += '<a style="padding: 8px;" class="' + tag_class + '" href="/status/' + d.data[i].id + '"><div class="mdui-chip">\n' +
+                        '    <span class="mdui-chip-title">' + d.data[i].key + '[' + d.data[i].ago_text + ']' + '</span>\n' +
+                        '  </div></a>';
                 }
                 status_list_sel.html(html);
             }, 'json');
