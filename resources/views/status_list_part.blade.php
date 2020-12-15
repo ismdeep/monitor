415e90b4e7e9a9e8c7678d29549abafc4d6b9014
L. Jiang <l.jiang.1024@gmail.com>
2020-12-15 17:53:46 +0800

    重构/status页面

--- resources/views/status_list_part.blade.php
+++ resources/views/status_list_part.blade.php
@@ -8,9 +8,11 @@
             $status['tag_class'] = 'mdui-text-color-red';
         @endphp
     @endif
-    <a style="padding: 8px;" class="{{$status['tag_class']}}" href="/status/{{$status['id']}}">
-        <div class="mdui-chip">
-            <span class="mdui-chip-title">{{$status['ago_text']}}</span>
-        </div>
-    </a>
+
+    <div class="mdui-chip">
+        <a style="padding: 8px;" class="{{$status['tag_class']}}" href="/status/{{$status['id']}}">
+            <span class="mdui-chip-title">{{$status['key']}} [{{$status['ago_text']}}]</span>
+        </a>
+    </div>
+
 @endforeach
